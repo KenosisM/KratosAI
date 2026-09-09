c5c914896bd34db18755d9d19fe07194be5f465f
Admin <13514030+smart-stuido_1_0@user.noreply.gitee.com>
2026-09-09 11:16:55 +0800

    修复一个语法错误

--- src/Ext/EffectType/Effect/StandEffect.cpp
+++ src/Ext/EffectType/Effect/StandEffect.cpp
@@ -109,7 +109,7 @@ TechnoStatus* StandEffect::SetupStandStatus()
 		status->MyMasterIsSpawned = masterIsSpawned;
 		// 额外附加AE
 		AttachEffect* standAEM = nullptr;
-		if (Data->Attach && TryGetAEManager(pStand, standAEM))
+		if (Data->Attach && TryGetAEManager<TechnoExt>(pStand, standAEM))
 		{
 			if (!Data->AttachEffects.empty())
 			{
